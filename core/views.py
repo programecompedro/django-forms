e20138ebd98ea2d671324789188e6fdb617b6006
pedro miguel <pdemeu@outlook.com>
2021-09-28 23:01:08 -0300

    Post Form do Contato

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .forms import ContatoForm
+from django.contrib import messages
 # Create your views here.
 def index(request):
     template='index.html'
@@ -7,7 +8,30 @@ def index(request):
 
 def contato(request):
     template='contato.html'
-    return render(request, template)
+    form = ContatoForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+
+            print('Mensagem Enviada ')
+            print(f'{nome}')
+            print(f'{email}')
+            print(f'{assunto}')
+            print(f'{mensagem}')
+
+            messages.success(request, 'E-mail Enviado com sucesso!')
+            form = ContatoForm()
+        else:
+            messages.error(request, 'Erro ao enviar E-mail')
+
+    context = {
+        "form": form
+    }
+    return render(request, template, context)
 
 def produto(request):
     template='produto.html' 
